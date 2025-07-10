bb5fb3b86521ba7d0b471dc37e359edcf4b570ac
giwoong <dev.choi0409@gmail.com>
2025-07-10 14:31:03 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/github/giwoong01/springapicommon/SpringApiCommonApplication.java
@@ -0,0 +1,13 @@
+package com.github.giwoong01.springapicommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiCommonApplication.class, args);
+    }
+
+}
